74efc1ad09af2803b8b27354fbd31ddad5d1c52f
Ransho <rafmaj94@gmail.com>
2017-03-24 08:12:47 +0100

    Will fail

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -10,4 +10,10 @@ public class LibraryTest {
         Library classUnderTest = new Library();
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
     }
+    
+    @Test public void testSomeLibraryMethod2() {
+    	System.out.println("Test: testSomeLibraryMethod()");
+        Library classUnderTest = new Library();
+        assertFalse("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
 }
