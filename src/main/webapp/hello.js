d130cdb4d91ba107cfef8f94acb3b56c586cf59b
casmonido <K.Komar@stud.elka.pw.edu.pl>
2017-04-30 18:37:08 +0200

    made a mistake

--- src/main/webapp/hello.js
+++ src/main/webapp/hello.js
@@ -1,6 +1,6 @@
 angular.module('demo', [])
 .controller('HelloJs', function($scope, $http) {
-    $http.get('http://127.0.0.1:9996/pik/greeting').
+    $http.get('http://127.0.0.1:9996/mywebapp/greeting').
         then(function(response) {
             $scope.greeting = response.data;
         });
