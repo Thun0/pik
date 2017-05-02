be53cc604f8a3b9dfe0816f371ac0adfb0935d40
casmonido <K.Komar@stud.elka.pw.edu.pl>
2017-05-02 23:46:49 +0200

    seems to work

--- src/main/java/hello/ServeIndex.java
+++ src/main/java/hello/ServeIndex.java
@@ -2,13 +2,19 @@ package hello;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ServeIndex {
+
+	@RequestMapping("/")
+	public String returnIndex() {
+		return "index.html";
+	}
 	
 	@RequestMapping("/app/**")
-	public String serveIndex() {
-		return "index";
+	public String redirectToIndex() {
+		return "forward:/";
 	}
 	
 }
