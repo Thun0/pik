0587ddcc5b9d9e06677be100de9762b7ab9c7c1c
ragdo <danrobmus@gmail.com>
2017-06-03 18:26:05 +0200

    PIK-7: Selected word details functionality
    Task-Url: http://127.0.0.1:8081/browse/PIK-7

--- src/main/java/hello/controller/EchoController.java
+++ src/main/java/hello/controller/EchoController.java
@@ -7,6 +7,8 @@ import java.io.InputStream;
 import java.nio.file.Paths;
 
 import javax.servlet.ServletContext;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -19,15 +21,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.xml.sax.SAXException;
 import org.springframework.oxm.castor.CastorMarshaller;
 import org.json.JSONObject;
 import org.json.XML;
 import org.junit.Test;
 import hello.MyFileReader;
+import hello.Synset;
 import hello.model.Echo;
+import hello.model.LinerCommand;
 import hello.ArrayList;
 import hello.LexicalUnit;
 import hello.XMLConverter;
+import hello.model.XMLReader;
 
 import org.exolab.castor.xml.XMLContext; 
 import org.exolab.castor.mapping.Mapping;
@@ -77,7 +83,7 @@ public class EchoController {
 	}
 	
 	@RequestMapping(method = RequestMethod.GET, produces = "application/json")
-	public ArrayList echo(@RequestParam(value="message", defaultValue="MESSAGE") String message) throws IOException, MappingException, MarshalException, ValidationException
+	public Synset echo(@RequestParam(value="message", defaultValue="MESSAGE") String message) throws IOException, MappingException, MarshalException, ValidationException, XPathExpressionException, SAXException, ParserConfigurationException
 	{
 //		Echo echo = new Echo(message);
 //		System.out.println(echo.getMessage());
@@ -85,23 +91,37 @@ public class EchoController {
 		File file = new ClassPathResource("test.xml").getFile();
 		File mapFile = new ClassPathResource("mapping.xml").getFile();
 
-		Mapping mapping = new Mapping();
-		mapping.loadMapping(mapFile.getAbsolutePath());
-		
-		XMLContext context = new XMLContext();
-		context.addMapping(mapping);
-		
-		
-		Unmarshaller unmarshaller = context.createUnmarshaller();
-		unmarshaller.setClass(ArrayList.class);
-		Marshaller marshaller = context.createMarshaller();
-		
-		XMLConverter converter = new XMLConverter();
-		converter.setMarshaller(marshaller);
-		converter.setUnmarshaller(unmarshaller);
-		
-		ArrayList laret = (ArrayList) converter.convertFromXMLToObject(file.getAbsolutePath());
+//		Mapping mapping = new Mapping();
+//		mapping.loadMapping(mapFile.getAbsolutePath());
+//		
+//		XMLContext context = new XMLContext();
+//		context.addMapping(mapping);
+//		
+//		
+//		Unmarshaller unmarshaller = context.createUnmarshaller();
+//		unmarshaller.setClass(ArrayList.class);
+//		Marshaller marshaller = context.createMarshaller();
+//		
+//		XMLConverter converter = new XMLConverter();
+//		converter.setMarshaller(marshaller);
+//		converter.setUnmarshaller(unmarshaller);
+//		
+//		ArrayList laret = (ArrayList) converter.convertFromXMLToObject(file.getAbsolutePath());
+		///////////////////////////////////
+		XMLReader xmlr = new XMLReader();
+		Synset ss = xmlr.searchForSynsetByLexicalUnit(file, "102034");
 
-		return laret;
+		return ss;
+	}
+	
+	@RequestMapping("/liner2")
+	public ArrayList<String> executeLiner2(@RequestParam(value="filepath", defaultValue="/opt/liner2.3/test/sentence.xml") String filepath) {
+		LinerCommand cmd = new LinerCommand(filepath);
+		try {
+			cmd.run();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return cmd.getTokens();
 	}
 }
